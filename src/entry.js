344cff5221b5d778388d1feae298b714f6ff941f
stage <1311012326@qq.com>
2018-05-04 13:58:40 +0800

    添加polyfill功能

--- src/entry.js
+++ src/entry.js
@@ -1,6 +1,9 @@
 import './css/font.less';
 import './css/mobile.css';
 import './css/style.scss';
+
+import 'core-js/fn/object/assign';
+import 'core-js/fn/string/repeat';
 import hello from './js/hello.js';
 import world from './js/world.js';
 
--- src/js/hello.js
+++ src/js/hello.js
@@ -2,4 +2,14 @@ export default function hello() {
 	let hello = document.createElement('p');
 	hello.textContent = `hello`;
 	document.querySelector("#root").appendChild(hello);
+	let data = {a:1};
+	Object.assign(data,{
+		b:2,
+		c:3,
+		d:4,
+	});
+	console.log("data:",data);
+
+	var x = '❀'.repeat(10)
+	console.log("x:",x)
 }
